93577368da4c80a9ff95f2c08bf0391a34dee18c
Krishna Singh <id.krishnasingh@gmail.com>
2025-07-08 08:24:13 +0530

    Reading configuration properties using @ConfigurationProperties

--- accounts/src/main/java/com/bank/accounts/controller/HelloWorldController.java
+++ accounts/src/main/java/com/bank/accounts/controller/HelloWorldController.java
@@ -6,6 +6,8 @@ import com.bank.accounts.service.impl.AccountsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,7 +49,7 @@ public class HelloWorldController {
 
 
     @GetMapping("/app-details")
-    public String getApplicationDetails(){
-        return applicationDetailsDto.message() + " \n " + applicationDetailsDto.contact() + " \n " + applicationDetailsDto.onCallSupport();
+    public ResponseEntity<ApplicationDetailsDto> getApplicationDetails(){
+        return ResponseEntity.status(HttpStatus.OK).body(applicationDetailsDto);
     }
 }
